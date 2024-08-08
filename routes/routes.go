e929f113e4c6c3b0af5d871b178346d514a8ff09
Irene Gu <gu.jiawen@northeastern.edu>
2024-08-08 15:58:17 -0400

    complete address update controller

--- routes/routes.go
+++ routes/routes.go
@@ -1,16 +1,16 @@
 package routes
 
 import (
-	"github.com/irenegujw/ecommerce-go/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/irenegujw/controllers"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 
-	incomingRoutes.POST("/users/signup",controllers.SignUp())
-	incomingRoutes.POST("/users/login",controllers.Login())
-	incomingRoutes.POST("/admin/addproduct",controllers.ProductViewerAdmin())
-	incomingRoutes.GET("/users/productview",controllers.searchProduct())
-	incomingRoutes.GET("/users/search",controllers.searchProductByQuery())
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.searchProduct())
+	incomingRoutes.GET("/users/search", controllers.searchProductByQuery())
 
 }
